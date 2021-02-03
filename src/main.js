19587c9409442445adaa5531cc95d09f0cbc4676
micDropper <ehahn307@gmail.com>
2021-02-02 23:35:25 -0500

    feat(sbootstrap): add bootstrap sframework and implement a design idea

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = true;
 
 new Vue({
   render: h => h(App)
