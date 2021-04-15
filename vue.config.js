ae093f63556c73c6d0a0a8486f10f439050e36f4
micDropper <ehahn307@gmail.com>
2021-04-14 23:08:38 -0400

    fix: add pwa config to vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/carpe-diem/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/carpe-diem/" : "/",
+  pwa: {
+    name: "carpe-diem",
+    workboxPluginMode: "GenerageSW"
+  }
 };
