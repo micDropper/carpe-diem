2bbc25cacbb7dfc67c852193533ba063e71a61fd
micDropper <ehahn307@gmail.com>
2021-04-16 21:59:27 -0400

    refactor: extract service-worker update toast into component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <template>
   <div class="container">
     <Home />
-    <b-toast
-      id="update-toast"
-      no-auto-hide
-      toaster="b-toaster-bottom-right"
-      header-class="d-none"
-    >
-      New content available!
-      <b-button @click="refreshApp" variant="primary" size="sm" class="mx-1"
-        >Refresh</b-button
-      >
-    </b-toast>
+    <update-toast />
   </div>
 </template>
 
 <script>
 import Home from "./components/Home.vue";
-import update from "./mixins/update";
+import UpdateToast from "./components/UpdateToast.vue";
 
 export default {
   name: "App",
   components: {
-    Home
-  },
-  mixins: [update],
-  watch: {
-    updateExists: function(newUpdateExists) {
-      if (newUpdateExists) {
-        this.$bvToast.show("update-toast");
-      } else {
-        this.$bvToast.hide("update-toast");
-      }
-    }
+    Home,
+    UpdateToast
   }
 };
 </script>
---
+++ src/components/UpdateToast.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-toast
+    id="update-toast"
+    no-auto-hide
+    toaster="b-toaster-bottom-right"
+    header-class="d-none"
+  >
+    New content available!
+    <b-button @click="refreshApp" variant="primary" size="sm" class="mx-1"
+      >Refresh</b-button
+    >
+  </b-toast>
+</template>
+
+<script>
+import update from "../mixins/update";
+
+export default {
+  name: "UpdateToast",
+  mixins: [update],
+  watch: {
+    updateExists: function(newUpdateExists) {
+      if (newUpdateExists) {
+        this.$bvToast.show("update-toast");
+      } else {
+        this.$bvToast.hide("update-toast");
+      }
+    }
+  }
+};
+</script>
